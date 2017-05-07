9e57cb3474d51f9910fc72cf448ab70f91142f6c
kieranhj <kieran@kconnell.co.uk>
2017-05-07 18:16:00 +0100

    MODE 1 plot routine
    Convert Apple II sprite data into colour using 3-bit lookup and plot in
    MODE 1.

--- pop-beeb.asm
+++ pop-beeb.asm
@@ -11,6 +11,7 @@ GUARD &8F
 .height     SKIP 1
 
 .yoffset    SKIP 1
+.yindex     SKIP 1
 
 .apple_count    SKIP 1
 .apple_byte     SKIP 1
@@ -19,9 +20,13 @@ GUARD &8F
 .beeb_count SKIP 1
 
 .bit_count  SKIP 1
+.pal_index  SKIP 1
+.pal_jump   SKIP 1
 
 ORG &E00
 GUARD &3000
+
+PLOT_MODE = 1
 
 .start
 
@@ -30,7 +35,7 @@ GUARD &3000
     \\ MODE 4
     LDA #22
     JSR &ffee
-    LDA #4
+    LDA #PLOT_MODE
     JSR &ffee
 
     \\ Parse chtab header
@@ -61,6 +66,7 @@ GUARD &3000
     CPX numimages
     BCC loop
 
+IF PLOT_MODE=4
     \\ Sprite plot
     LDA #LO(&6500)
     STA writeptr
@@ -69,6 +75,18 @@ GUARD &3000
 
     LDA #0
     JSR apple_plot_mode_4
+ENDIF
+
+IF PLOT_MODE=1
+    \\ Sprite plot
+    LDA #LO(&4A00)
+    STA writeptr
+    LDA #HI(&4A00)
+    STA writeptr+1
+
+    LDA #1
+    JSR apple_plot_mode_1
+ENDIF
 
     .return
     RTS
@@ -191,6 +209,233 @@ GUARD &3000
 
     .return
     RTS
+}
+
+.apple_plot_mode_1
+{
+    ASL A
+    TAX
+    LDA chtab1+1, X
+    STA readptr
+    LDA chtab1+2, X
+    STA readptr+1
+
+    LDY #0
+    LDA (readptr), Y
+    STA width
+    INY
+    LDA (readptr), Y
+    STA height
+
+    CLC
+    LDA readptr
+    ADC #2
+    STA init_addr + 1
+    STA sprite_addr + 1
+    LDA readptr+1
+    ADC #0
+    STA init_addr + 2
+    STA sprite_addr + 2
+
+
+    LDX #0          ; data index
+    LDY #7          ; yoffset
+
+    .yloop
+    STY yoffset
+    STY yindex
+
+    LDA width
+    STA apple_count
+
+    LDA #0
+    STA beeb_byte
+    STA pal_index
+    STA pal_jump
+
+    LDA #4
+    STA beeb_count
+
+    \\ Initialise palindex
+    .init_addr
+    LDA &FFFF, X
+    STA apple_byte
+
+    LSR apple_byte
+    ROL pal_index
+
+    LDA #6              ; we just consumed one
+    STA bit_count
+
+    .xloop
+
+    .bit_loop    
+    LSR apple_byte       ; rotate bottom bit into Carry
+    ROL pal_index        ; rotate carry into palette index
+
+    ; look up colour pixel
+    LDA pal_index
+    AND #&7
+    STA pal_index
+    ORA pal_jump
+    TAY
+    LDA pal_table,Y
+    ORA beeb_byte
+    STA beeb_byte
+
+    LDA pal_jump
+    CLC
+    ADC #8
+    STA pal_jump
+
+    DEC beeb_count
+    BNE next_bit
+
+    \\ Write byte to screen
+    LDY yindex
+    LDA beeb_byte
+    STA (writeptr), Y
+
+    \\ Next screen column
+    TYA
+    CLC
+    ADC #8
+    STA yindex
+
+    LDA #0
+    STA beeb_byte
+    STA pal_jump
+
+    LDA #4
+    STA beeb_count
+
+    .next_bit
+    DEC bit_count
+    BNE bit_loop
+
+    \\ Next apple byte
+
+    INX                 ; next apple byte
+
+    .sprite_addr
+    LDA &FFFF, X
+    STA apple_byte
+
+    LDA #7
+    STA bit_count
+
+    DEC apple_count
+    BNE xloop
+
+    \\ Flush Beeb byte to screen if needed
+    LDA beeb_count
+    CMP #4
+    BEQ done_row
+
+    .flushloop
+    ASL pal_index        ; rotate zero into palette index
+
+    ; look up colour pixel
+    LDA pal_index
+    AND #&7
+    STA pal_index
+    ORA pal_jump
+    TAY
+    LDA pal_table,Y
+    ORA beeb_byte
+    STA beeb_byte
+
+    LDA pal_jump
+    CLC
+    ADC #8
+    STA pal_jump
+
+    DEC beeb_count
+    BNE flushloop
+
+    LDY yindex
+    LDA beeb_byte
+    STA (writeptr), Y
+    
+    .done_row
+    DEC height
+    BEQ done
+
+    LDY yoffset
+    DEY
+    BPL jump_yloop
+    
+    \\ Next char row
+
+    SEC
+    LDA writeptr
+    SBC #LO(640)
+    STA writeptr
+    LDA writeptr+1
+    SBC #HI(640)
+    STA writeptr+1
+
+    LDY #7
+    .jump_yloop
+    JMP yloop
+
+    .done
+
+    .return
+    RTS
+}
+
+MACRO MODE1_PIXEL colour, shift
+    EQUB (((colour AND 2) * 8) OR (colour AND 1)) << shift
+ENDMACRO
+
+.pal_table
+.pal_table_even3
+{
+    MODE1_PIXEL 0, 3
+    MODE1_PIXEL 0, 3
+    MODE1_PIXEL 2, 3
+    MODE1_PIXEL 3, 3
+    MODE1_PIXEL 0, 3
+    MODE1_PIXEL 1, 3
+    MODE1_PIXEL 3, 3
+    MODE1_PIXEL 3, 3
+}
+
+.pal_table_odd2
+{
+    MODE1_PIXEL 0, 2
+    MODE1_PIXEL 0, 2
+    MODE1_PIXEL 1, 2
+    MODE1_PIXEL 3, 2
+    MODE1_PIXEL 0, 2
+    MODE1_PIXEL 2, 2
+    MODE1_PIXEL 3, 2
+    MODE1_PIXEL 3, 2
+}
+
+.pal_table_even1
+{
+    MODE1_PIXEL 0, 1
+    MODE1_PIXEL 0, 1
+    MODE1_PIXEL 2, 1
+    MODE1_PIXEL 3, 1
+    MODE1_PIXEL 0, 1
+    MODE1_PIXEL 1, 1
+    MODE1_PIXEL 3, 1
+    MODE1_PIXEL 3, 1
+}
+
+.pal_table_odd0
+{
+    MODE1_PIXEL 0, 0
+    MODE1_PIXEL 0, 0
+    MODE1_PIXEL 1, 0
+    MODE1_PIXEL 3, 0
+    MODE1_PIXEL 0, 0
+    MODE1_PIXEL 2, 0
+    MODE1_PIXEL 3, 0
+    MODE1_PIXEL 3, 0
 }
 
 
