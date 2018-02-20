86fe727c563ad04434cd029fcfe85eaf41800135
kieranhj <kieran@kconnell.co.uk>
2018-02-20 18:42:09 +0000

    Auto generated build number
    Derive build numbers from date, time and username in make.bat
    Reordered files on SIDE A according to load order

--- make.bat
+++ make.bat
@@ -1,3 +1,4 @@
 @echo off
+echo EQUB $%DATE:~8,2%, $%DATE:~3,2%, $%DATE:~0,2%, $%TIME:~0,2%, $%TIME:~3,2%, "%USERNAME:~0,2%" > version.txt
 ..\..\bin\BeebAsm.exe -v -i pop-beeb.asm -do pop-beeb-side-a.ssd -boot Core > compile.txt
 bin\bbcim -interss sd pop-beeb-side-a.ssd pop-beeb-side-b.ssd pop-beeb.dsd
