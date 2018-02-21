2e6f5489c099f4835a6c63a8a0e31e81aac6ad00
kieranhj <kieran@kconnell.co.uk>
2018-02-21 08:48:32 +0000

    New fat guard sprites
    Also fixed up build number before 10am!

--- make.bat
+++ make.bat
@@ -1,4 +1,6 @@
 @echo off
-echo EQUB $%DATE:~8,2%, $%DATE:~3,2%, $%DATE:~0,2%, $%TIME:~0,2%, $%TIME:~3,2%, "%USERNAME:~0,2%" > version.txt
+set HH=%TIME:~0,2%
+if %HH% leq 9 set HH=0%HH:~1,1%
+echo EQUB $%DATE:~8,2%, $%DATE:~3,2%, $%DATE:~0,2%, $%HH%, $%TIME:~3,2%, "%USERNAME:~0,2%" > version.txt
 ..\..\bin\BeebAsm.exe -v -i pop-beeb.asm -do pop-beeb-side-a.ssd -boot Core > compile.txt
 bin\bbcim -interss sd pop-beeb-side-a.ssd pop-beeb-side-b.ssd pop-beeb.dsd
