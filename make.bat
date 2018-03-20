d69f1f4467e376fd0f0bb40394a0488f1ad741e8
kieranhj <kieran@kconnell.co.uk>
2018-03-20 16:35:12 +0000

    Sorted disc layout
    Side A & B now have titles and version number
    Also spaced out LEVEL files on side B to be on track boundaries

--- make.bat
+++ make.bat
@@ -3,7 +3,8 @@ set HH=%TIME:~0,2%
 if %HH% leq 9 set HH=0%HH:~1,1%
 echo EQUB $%DATE:~8,2%, $%DATE:~3,2%, $%DATE:~0,2%, $%HH%, $%TIME:~3,2%, "%USERNAME:~0,2%" > version.txt
 del pop-beeb-side-a.ssd
-..\..\bin\BeebAsm.exe -v -i pop-beeb.asm -do pop-beeb-side-a.ssd -boot Core > compile.txt
+bin\BeebAsm.exe -v -i pop-beeb.asm -di disc/template-side-a.ssd.bin -do pop-beeb-side-a.ssd > compile.txt
+bin\bbcim -a "pop-beeb-side-a.ssd" "disc/boot.txt"
 
 if %ERRORLEVEL% neq 0 (
 	echo Assemble failed!
