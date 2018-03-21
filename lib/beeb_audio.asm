1df407d8a78b9338df93958064776d317c6c5254
kieranhj <kieran@kconnell.co.uk>
2018-03-21 10:54:44 +0000

    Added new tunes
    story2, story3, killedjaffar
    Not enough space for story4 in attract cutscene audio bank.  Need to
    figure out solution.

--- lib/beeb_audio.asm
+++ lib/beeb_audio.asm
@@ -289,12 +289,12 @@ ENDIF ; _AUDIO_DEBUG
     EQUW pop_music_intro;, &8080 ; s_Presents = 1
     EQUW 0;, &8080 ; s_Byline = 2
     EQUW 0;, &8080 ; s_Title = 3
-    EQUW 0;, &8080 ; s_Prolog = 4
-    EQUW 0;, &8080 ; s_Sumup = 5
+    EQUW pop_music_prolog;, &8080 ; s_Prolog = 4
+    EQUW pop_music_sumup;, &8080 ; s_Sumup = 5
     EQUW 0; there is no 6
     EQUW pop_music_princess;, &8080 ; s_Princess = 7
     EQUW 0;, &8080 ; s_Squeek = 8
-    EQUW 0;, &8080 ; s_Vizier = 9
+    EQUW pop_music_enters;, &8080 ; s_Vizier = 9
     EQUW 0;, &8080 ; s_Buildup = 10
     EQUW pop_music_leaves;, &8080 ; s_Magic = 11
     EQUW 0;, &8080 ; s_StTimer = 12
--- lib/beeb_audio_banks.asm
+++ lib/beeb_audio_banks.asm
@@ -11,8 +11,8 @@ GUARD ANDY_TOP
 .pop_audio_bank0_start
 .pop_music_intro
 INCBIN "audio/ip/m-intro-wrongsignaturevgm.raw.exo"
-.pop_music_sumup
-INCBIN "audio/ip/m-story5-end-merge-bla.raw.exo"
+.pop_music_prolog
+INCBIN "audio/ip/m-story1.raw.exo"
 .pop_audio_bank0_end
 SAVE "Audio0", pop_audio_bank0_start, pop_audio_bank0_end, 0
 
@@ -32,8 +32,12 @@ GUARD ANDY_TOP
 .pop_audio_bank1_start
 .pop_music_princess
 INCBIN "audio/ip/m-story2.raw.exo"
+.pop_music_enters
+INCBIN "audio/ip/m-story3.raw.exo"
 .pop_music_leaves
-INCBIN "audio/ip/m-story4.raw.exo"
+;INCBIN "audio/ip/m-story4.raw.exo"
+.pop_music_sumup
+INCBIN "audio/ip/m-story5-end-merge-bla.raw.exo"
 .pop_audio_bank1_end
 SAVE "Audio1", pop_audio_bank1_start, pop_audio_bank1_end, 0
 
