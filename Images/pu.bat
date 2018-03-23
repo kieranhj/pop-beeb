625a31a2b9d3f34953af85717abdc0cda9a65ab4
kieranhj <kieran@kconnell.co.uk>
2018-03-23 23:45:45 +0000

    Checking pudecrunch destination works OK

--- Images/pu.bat
+++ Images/pu.bat
@@ -1,5 +1,5 @@
 @echo off
-..\bin\pucrunch.exe -d -c0 -l0x8000 BEEB.IMG.CHTAB6.mode2.bin chtab6.pu.bin
-..\bin\pucrunch.exe -d -c0 -l0x9C00 BEEB.IMG.CHTAB7.mode2.bin chtab7.pu.bin
-..\bin\pucrunch.exe -d -c0 -l0x8000 BEEB.IMG.CHTAB8.mode2.bin chtab8.pu.bin
-..\bin\pucrunch.exe -d -c0 -l0x8000 BEEB.IMG.CHTAB9.mode2.bin chtab9.pu.bin
+..\bin\pucrunch.exe -d -c0 -l0x1000 BEEB.IMG.CHTAB6.mode2.bin chtab6.pu.bin
+..\bin\pucrunch.exe -d -c0 -l0x1000 BEEB.IMG.CHTAB7.mode2.bin chtab7.pu.bin
+..\bin\pucrunch.exe -d -c0 -l0x1000 BEEB.IMG.CHTAB8.mode2.bin chtab8.pu.bin
+..\bin\pucrunch.exe -d -c0 -l0x1000 BEEB.IMG.CHTAB9.mode2.bin chtab9.pu.bin
