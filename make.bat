264c19576463da0b582451102d35cfffaa68f60f
SimonM <podvilla@gmail.com>
2018-03-24 13:54:16 +0000

    Selfish change to not pipe assembler output

--- make.bat
+++ make.bat
@@ -3,7 +3,10 @@ set HH=%TIME:~0,2%
 if %HH% leq 9 set HH=0%HH:~1,1%
 echo EQUB $%DATE:~8,2%, $%DATE:~3,2%, $%DATE:~0,2%, $%HH%, $%TIME:~3,2%, "%USERNAME:~0,2%" > version.txt
 del pop-beeb-side-a.ssd
-bin\BeebAsm.exe -v -i pop-beeb.asm -di disc/template-side-a.ssd.bin -do pop-beeb-side-a.ssd > compile.txt
+
+rem SM: selfishly not sending compiler output to compile.txt so I can use VS.code console instead
+rem bin\BeebAsm.exe -v -i pop-beeb.asm -di disc/template-side-a.ssd.bin -do pop-beeb-side-a.ssd > compile.txt
+bin\BeebAsm.exe -v -i pop-beeb.asm -di disc/template-side-a.ssd.bin -do pop-beeb-side-a.ssd 
 
 if %ERRORLEVEL% neq 0 (
 	echo Assemble failed!
